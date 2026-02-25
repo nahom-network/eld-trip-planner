8cd55cc5657bf656b7a17a326f0b4eb32a92f6a1
nahom <nahom.d54@gmail.com>
2026-02-25 22:16:05 +0300

    fix(entrypoint): remove --clear option from collectstatic command

--- trip_planner/entrypoint.sh
+++ trip_planner/entrypoint.sh
@@ -5,7 +5,7 @@ echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
 exec gunicorn trip_planner.wsgi:application \
